0c097b4e6968d11375548580fa54f34c8b3142b5
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-14 12:34:23 +0600

    tags.json api created

---
+++ src/pages/Home/Tags/Tags.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const Tags = () => {
+    return (
+        <section className='tags container mx-auto px-6 py-14'>
+            <SectionTitle title="Popular Tags" sub_title="Explore Topics of Interest and Join the Conversation" />
+        </section>
+    );
+};
+
+export default Tags;
