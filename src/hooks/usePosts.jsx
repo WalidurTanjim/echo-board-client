69a460a0804ac14ff95f4e7192a8f354da9c19e8
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-14 14:23:12 +0600

    usePosts hook created to fetch all posts

---
+++ src/hooks/usePosts.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import useAxiosPublic from './useAxiosPublic';
+
+const usePosts = () => {
+    const axiosPublic = useAxiosPublic();
+
+    const { data: posts = [], isPending, isError, error, refetch } = useQuery({
+        queryKey: ['posts'], 
+        queryFn: async() => {
+            const res = await axiosPublic.get('/posts');
+            const data = await res?.data;
+            return data;
+        }
+    })
+
+    return [ posts, isPending, isError, error, refetch ];
+};
+
+export default usePosts;
---
+++ src/pages/Home/Posts/Posts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Posts = () => {
+    return (
+        <section className='posts container mx-auto px-6 py-14'>
+            
+        </section>
+    );
+};
+
+export default Posts;
