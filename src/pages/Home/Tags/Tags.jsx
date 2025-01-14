19908dc818d448146cb957d2ddad9d2cdda78423
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-14 13:16:20 +0600

    tags section done

--- src/pages/Home/Tags/Tags.jsx
+++ src/pages/Home/Tags/Tags.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import useTags from '../../../hooks/useTags';
+import Spinner from '../../../components/Spinner/Spinner';
 
 const Tags = () => {
-    
+    const [tags, isPending, isError, error] = useTags();
+    console.log(tags);
 
     return (
         <section className='tags container mx-auto px-6 py-14'>
             <SectionTitle title="Popular Tags" sub_title="Explore Topics of Interest and Join the Conversation" />
 
-            <div className="pt-14 grid gap-5 grid-cols-4">
-
+            <div className="">
+                {
+                    isPending ? (
+                        <Spinner />
+                    ) : isError ? (
+                        <div>
+                            <h1 className='text-2xl font-medium text-red-600 text-center'>{error?.message}</h1>
+                        </div>
+                    ) : (
+                        <div className='grid gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'>
+                            {
+                                tags?.map(tag => {
+                                    const { _id, tag_name, tag_icon } = tag;
+                                    return (
+                                        <div key={_id} className='p-2 border rounded-md flex gap-2 cursor-pointer group hover:border-blue-300 hover:shadow-lg hover:text-blue-600'>
+                                            <img src={tag_icon} alt="" className='w-[21px] h-[21px]' />
+                                            <h1 className='text-sm'>{tag_name}</h1>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                }
             </div>
         </section>
     );
