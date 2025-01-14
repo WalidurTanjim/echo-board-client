8763e05177ea67098a2e4876bfa3bcd7a1977882
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-14 12:06:16 +0600

    Banner created with anication

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <section className='home'>
-            <h1>Home</h1>
+            <div className='py-10 bg-gradient-to-r from-[#fafcff] to-[#edf2fc]'>
+                <Banner />
+            </div>
         </section>
     );
 };
