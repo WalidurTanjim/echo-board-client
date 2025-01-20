7cd10c35a720bc743f1e937e184fdff03e3f9fcb
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-20 16:54:29 +0600

    announcement created to the home page

---
+++ src/components/AnnouncementRow/AnnouncementRow.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const AnnouncementRow = ({ announcement }) => {
+    const { _id, announcementDescription, authorName, authorImage, postTime, title } = announcement;
+
+    return (
+        <div className='announcement p-2 rounded-md border hover:border-blue-200 group'>
+            {/* author info div starts */}
+            <div className="flex gap-2">
+                <img src={authorImage} alt="" className='w-[40px] h-[37px] rounded-md' />
+
+                <div>
+                    <h3 className='text-sm text-slate-800 font-medium'>{authorName}</h3>
+                    <p className='text-xs text-gray-500'>{postTime}</p>
+                </div>
+            </div>
+
+            <div className="my-2">
+                <hr />
+            </div>
+
+            {/* announcement */}
+            <div className="">
+                <h1 className='text-lg font-semibold text-slate-700 group-hover:text-blue-500 group-hover:text-hover-500'>{title}</h1>
+
+                <p className='text-sm text-gray-500 mt-1'>{announcementDescription}</p>
+            </div>
+        </div>
+    );
+};
+
+export default AnnouncementRow;
--- src/pages/Home/Announcements/Announcements.jsx
+++ src/pages/Home/Announcements/Announcements.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import useAnnouncement from '../../../hooks/useAnnouncement';
+import Spinner from '../../../components/Spinner/Spinner';
+import AnnouncementRow from '../../../components/AnnouncementRow/AnnouncementRow';
 
 const Announcements = () => {
+    const [ announcements, isPending, isError, error, refetch ] = useAnnouncement();
+
     return (
         <section className='announcement container mx-auto px-6 py-14'>
             <SectionTitle title="Announcement" sub_title="Latest Announcements and Updates" />
+
+            {
+                isPending ? (
+                    <Spinner />
+                ) : isError ? (
+                    <div className='flex items-center justify-center py-14 border rounded-md'>
+                        <h1 className='text-xl font-medium text-red-500'>{error?.message}</h1>
+                    </div>
+                ) : 
+                <div>
+                    {
+                        announcements.length > 3 ?
+                        announcements.slice(0, 3)?.map(announcement => <AnnouncementRow key={announcement?._id} announcement={announcement} />) :
+                        announcements?.map(announcement => <AnnouncementRow key={announcement?._id} announcement={announcement} />)
+                    }
+                </div>
+            }
+
+            <div className='mt-7 flex items-center justify-center'>
+                <button type="submit" className="py-2 px-6 inline-flex items-center justify-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 active:bg-blue-100 focus:outline-none focus:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:bg-blue-800/30 dark:hover:bg-blue-800/20 dark:focus:bg-blue-800/20">Show More</button>
+            </div>
         </section>
     );
 };
