3eaa0a0e0d408a605a9df0341fe1d5e77eeca042
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-22 14:40:10 +0600

    payment method added successfully

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -2,8 +2,9 @@ import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React, { useEffect, useState } from 'react';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
 import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
 
-const Checkout = () => {
+const Checkout = ({ totalPayment }) => {
     const [errMsg, setErrMsg] = useState('');
     const [clientSecret, setClientSecret] = useState('');
     const stripe = useStripe();
@@ -13,42 +14,71 @@ const Checkout = () => {
 
     // useEffect
     useEffect(() => {
-        const fetchData = async() => {
-            try{
-                const res = await axiosSecure.post(`/create-payment-intent`);
+        const fetchData = async () => {
+            try {
+                const res = await axiosSecure.post(`/create-payment-intent`, { price: totalPayment });
                 const data = await res?.data?.clientSecret;
                 setClientSecret(data);
-            }catch(err){
+            } catch (err) {
                 console.error(err);
             }
         };
         fetchData();
-    }, [axiosSecure]);
+    }, [axiosSecure, totalPayment]);
 
-    // handleSubmit
+    // handleSubmit for pay money
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         setErrMsg('');
 
-        if(!stripe || !elements) return
+        if (!stripe || !elements) return
 
         const card = elements.getElement(CardElement);
 
-        if(card == null) return
+        if (card == null) return
 
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
             card,
         });
 
-        if(error){
+        if (error) {
             setErrMsg(error?.message);
-            console.error("[Error]", error);
-        }else{
+            // console.error("[Error]", error);
+        } else {
             console.log("[Payment method]", paymentMethod);
         }
-        
+
+
+        // confirm payment
+        const { paymentIntent, error: confirmCardPaymentError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: user?.displayName || "anonymous",
+                    email: user?.email || "anonymous"
+                }
+            }
+        })
+
+        if (confirmCardPaymentError) {
+            // console.error("[Confirm card payment error]:", confirmCardPaymentError);
+            Swal.fire({
+                title: "Oops!",
+                text: "There is a problem. Try again.",
+                icon: "error"
+            });
+        } else {
+            // console.log("[Confirm card payment intent]:", paymentIntent);
+            if (paymentIntent?.status === 'succeeded') {
+                Swal.fire({
+                    title: "Congratulations 🎉",
+                    text: `${user?.displayName} become GOLD badge user`,
+                    icon: "success"
+                });
+            }
+        }
 
     }
 
@@ -71,7 +101,7 @@ const Checkout = () => {
                 }}
             />
 
-            { errMsg ? <p className='mt-3 text-sm text-red-600 ms-1'>** {errMsg}</p> : undefined }
+            {errMsg ? <p className='mt-3 text-sm text-red-600 ms-1'>** {errMsg}</p> : undefined}
             <button type="submit" disabled={!stripe || !clientSecret} className="py-2 px-6 mt-3 inline-flex items-center justify-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 active:bg-blue-100 focus:outline-none focus:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:bg-blue-800/30 dark:hover:bg-blue-800/20 dark:focus:bg-blue-800/20">
                 Pay
             </button>
--- src/pages/Membership/Membership.jsx
+++ src/pages/Membership/Membership.jsx
@@ -8,13 +8,20 @@ import Checkout from '../Checkout/Checkout';
 const striptPromise = loadStripe(import.meta.env.VITE_strp_pk);
 
 const Membership = () => {
+    const totalPayment = 50;
+
     return (
         <section className="membership container mx-auto px-6 py-14">
             <SectionTitle title="Membership" sub_title="Unlock Exclusive Benefits with Our Membership Plans" />
 
+            {/* money quantity */}
+            <div className="mb-10">
+                <h1 className='text-xl font-medium text-slate-700'>Total payment: $<span className='text-blue-500 font-medium'>{totalPayment}</span></h1>
+            </div>
+
             <div>
                 <Elements stripe={striptPromise}>
-                    <Checkout />
+                    <Checkout totalPayment={totalPayment} />
                 </Elements>
             </div>
         </section>
