1d13b2390e96a99d95f72fdecd2336329c7d87ef
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-22 17:39:39 +0600

    GoogleSignIn added

--- src/components/DashboardRoutes/DashboardRoutes.jsx
+++ src/components/DashboardRoutes/DashboardRoutes.jsx
@@ -17,7 +17,7 @@ const DashboardRoutes = () => {
         <div className="relative border">
             {/* Toggle Button */}
             <div className="text-center">
-                <div type="button" className="absolute top-5 right-5 py-2 px-2 border bg-transparent text-white text-sm font-medium rounded-full hover:bg-gray-100 active:bg-transparent focus:outline-none focus:ring-2 focus:ring-transparent" onClick={toggleSidebar}>
+                <div type="button" className="absolute top-5 right-5 py-2 px-2 border bg-slate-500 text-white text-sm font-medium rounded-full hover:bg-slate-400 active:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-transparent" onClick={toggleSidebar}>
                     {
                         isSidebarOpen ?
                         <XMarkIcon className="size-5 text-slate-900" /> : 
