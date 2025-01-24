7b7304f3b527a39936a10a31099188a648d0f56b
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-01-24 16:19:13 +0600

    vercel live link added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import React from 'react';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    // baseURL: 'http://localhost:5000',
+    baseURL: 'https://echo-board-server.vercel.app',
     withCredentials: true
 })
 
